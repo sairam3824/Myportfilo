e426f4ef66839d778f62c870e34a33b55ce7afcd
MARURI SAI RAMA LINGA REDDY <Msrlreddy@outlook.com>
2025-10-02 10:59:46 +0530

    modified:   assets/js/main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,18 +72,20 @@ function scrollActive() {
       sectionTop = current.offsetTop - 58,
       sectionId = current.getAttribute("id");
 
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector(".nav__menu a[href*=" + sectionId + "]")
-        .classList.add("active-link");
-    } else {
-      document
-        .querySelector(".nav__menu a[href*=" + sectionId + "]")
-        .classList.remove("active-link");
+    const selector = `.nav__menu a[href*="${sectionId}"]`;
+    const link = document.querySelector(selector);
+
+    if (link) {
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+        link.classList.add("active-link");
+      } else {
+        link.classList.remove("active-link");
+      }
     }
   });
 }
 window.addEventListener("scroll", scrollActive);
+
 
 /*=============== LIGHT DARK THEME ===============*/
 const themeButton = document.getElementById("theme-button");
